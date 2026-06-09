1aa6527115f7709a870dc54531e6e0d63cd40687
LiamPochin <192381328+LiamPochin@users.noreply.github.com>
2026-06-09 09:41:29 +0100

    Added Foodbank Registration Page Added register.html, register.css and register.js.

---
+++ public/js/register.js
@@ -0,0 +1,8 @@
+let myForm = document.querySelector('form');
+let myName = document.querySelector('#name');
+
+myForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    confirmMessage.TextContent = `Hi ${myName.value}, your 
+    foodbank has been registered.`;
+});
